9f337c666d7947ef33e3a854620b949ce1d8be27
Steven Scott <Contact@TheDgtl.net>
2012-03-04 00:10:08 -0800

    [Version 2.5.3]   - Updated iChatMeEvent for new event system

--- src/net/TheDgtl/iChat/iChatMeEvent.java
+++ src/net/TheDgtl/iChat/iChatMeEvent.java
@@ -2,10 +2,21 @@ package net.TheDgtl.iChat;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 
 public class iChatMeEvent extends Event {
 	private String message;
 	private Player player;
+	
+	private static final HandlerList handlers = new HandlerList();
+	
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 	
 	public iChatMeEvent(final Player player, final String message) {
 		this.player = player;
